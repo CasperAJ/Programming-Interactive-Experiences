ee1a15755f9471937b24e12f977d2c0c4e7fd6f7
Casper <CasperAJ@users.noreply.github.com>
2019-01-01 15:00:23 +0100

    Updated with forgotten comments

--- main/main.ino
+++ main/main.ino
@@ -39,46 +39,46 @@ void loop()
   delay(1);
   boolean buttonState = !digitalRead(D6); // Gives true when button is pressed/held down
   //Serial.println(buttonState);
-  startTimer = millis(); // Creates timer || Gives times since the Arduino board was activated
+  startTimer = millis(); // Creates timer || Gives times since the Arduino board was activated || This behavior is repeated for similar variables
 
   if (buttonState == true && buttonStateOld == false) // Button is pressed
   {
     timerPressed = millis(); // Gets the time since the board started of the time when the button is pressed
     timerLastPressed = millis(); // Gets the number of millisec since the board started, at the instant button is clicked // eg value 2866, its been 2866 seconds since the board was started. timerLastPressed's value is 2866 further on.
-    resetCount = 0;
+    resetCount = 0; // Reset the reset counter
   }
 
   if (buttonState == false && buttonStateOld == true) // Button is released
   {
-    timesPressed[(timesPressedPosition++) % numberOfPresses] = millis() - timerPressed;
-    timerLastPressed = millis();
+    timesPressed[(timesPressedPosition++) % numberOfPresses] = millis() - timerPressed; // 
+    timerLastPressed = millis(); // Gets the time since the button was last pressed
   }
-  buttonStateOld = buttonState;
+  buttonStateOld = buttonState; // Changes the buttons state, so that i holds the late state in StateOld
 
 
   if ( (startTimer - timerLastPressed) > resetTimer && hasBeenUnlocked == true) // Reset button push recording session
   {
-    Serial.println("Reset made");
-    timesPressedPosition = 0;
-    timerLastPressed = millis();
-    resetCount++;
-    Serial.println(resetCount);
+    Serial.println("Reset made"); // Prints out reset made
+    timesPressedPosition = 0; // Reset PressedPosition
+    timerLastPressed = millis(); // Gets a new LastPressed timer
+    resetCount++; // Increases the reset counter with 1
+    Serial.println(resetCount); // Prints out the reset count
   }
 
   if (timesPressedPosition == numberOfPresses) // Verify pattern
   {
-    int clearedPattern = 0;
-    for (int i = 0; i < numberOfPresses; i ++)
+    int clearedPattern = 0; // Set and create a ClearedPattern counter variable
+    for (int i = 0; i < numberOfPresses; i ++) // Continues as long as I has a value lower than numberOfPresses
     {
-      int pressedPatterenValue = timesPressed[i];
-      int ppv = pressedPatterenValue;
+      int pressedPatterenValue = timesPressed[i]; // Set pressedPatterenValue to be equal to the value of timesPressed of I || eg timesPressed[2], value on index nr 2
+      int ppv = pressedPatterenValue; // No actual function besides decreasing the number of characters I need to write to get the value (Im lazy but still want proper naming as much as possible)
 
-      int unlockPatterenValueForI = unlockPatteren[i];
-      int upv = unlockPatterenValueForI;
-      int lowerupv = (upv - unlockPatternBufferZone);
-      int higherupv = (upv + unlockPatternBufferZone);
+      int unlockPatterenValueForI = unlockPatteren[i]; // Set unlockPatterenValueForI to be equal to the correct value for the index of I
+      int upv = unlockPatterenValueForI; // No actual function, Im still just lazy
+      int lowerupv = (upv - unlockPatternBufferZone); // Creates the lower acceptable unlock value by saing the actual unlock value minus the buffer
+      int higherupv = (upv + unlockPatternBufferZone); // Creates upper accetable value, same way as the line above
 
-      if ( lowerupv <= ppv && ppv <= higherupv )
+      if ( lowerupv <= ppv && ppv <= higherupv ) // Check if the ppv is withing the lower and upper acceptable values
       {
         Serial.print("Success    ");
         Serial.print(lowerupv);
@@ -87,21 +87,23 @@ void loop()
         Serial.print(" |||| ");
         Serial.println(higherupv);
         clearedPattern++;
+        // The abovr, prints out data if the ppv-value is acceptable and increase clearedPattern by 1
       }
-      else
+      else // if ppv is notacceptable is prints our the correct value for that index spot
       {
         Serial.println(timesPressed[i]);
       }
     }
     //////////////////////////////////////////////////////////////////
-    if(clearedPattern == numberOfPresses) { Unlock(true); }
+    if(clearedPattern == numberOfPresses) { Unlock(true); } // if clearedPattern count is equal to numberOfPResses use the Unlock method
     //////////////////////////////////////////////////////////////////
-    timesPressedPosition = 0;
-    Serial.println();
+    timesPressedPosition = 0; // Reset timesPressedPosition
+    Serial.println(); // Print empty line
   }
 
     //////////////////////////////////////////////////////////////////
     if( resetCount == desiredResetCount && hasBeenUnlocked == true) { resetCount = 0; Lock(true); }
+    // if resetCount is equal to the desriedResetCount and the lock has been unlocked, then reset resetCount and lock the lock
     //////////////////////////////////////////////////////////////////
 
 }
